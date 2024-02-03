45c9be1e91245f4a62c838b92ffe6757cde010ae
rabin osti <emperoromega10@gmail.com>
2024-02-03 17:17:34 +0545

    minor ui

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./contact.css";
+const Contact = () => {
+  return (
+    <div className="contact">
+      <h2>&nbsp;&nbsp;Contact Us&nbsp;&nbsp;</h2>
+    </div>
+  );
+};
+
+export default Contact;
